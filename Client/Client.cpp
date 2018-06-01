fd7eae1fa7d2c7f30232f4b3bd643244678415ca
Flavio <flaviodss63@gmail.com>
2018-06-01 19:23:58 -0300

    Initial

---
+++ Client/Client.cpp
@@ -0,0 +1,69 @@
+#include <Windows.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	//Tamanho do Buffer
+	const int TamanhoBuffer = 512;
+
+	//Buffer para armazenar a informação recebida do servidor.
+	char buffer[TamanhoBuffer];
+
+	//Mensagem a ser enviada
+	string mensagemEnviada = "";
+
+	//Nome do pipe
+	string pipeName = "\\\\.\\pipe\\MeuPipe";
+
+	////Handler do pipe
+	HANDLE hPipe;
+
+	cout << "Tentando conectar-se ao servidor..." << endl;
+
+	while (true) {
+		//Tenta obter uma conexão com o named pipe
+		hPipe = CreateFile(pipeName.c_str(), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+
+		//Se não tiver sucesso na conexão, espera 5 segundos para tentar se conectar novamente
+		if (hPipe == INVALID_HANDLE_VALUE) {
+			cout << "Tenta se conectar novamente em 5 segundos..." << endl;
+			Sleep(1000);
+		}
+		else {
+			cout << "Conectado!" << endl;
+			break;
+		}
+	}
+
+	cout << "Digite o texto a ser enviado: ";
+
+	//Captura a entrada do usuário
+	getline(cin, mensagemEnviada);
+
+	while (mensagemEnviada != "sair") {
+		DWORD tamanhoMensagemEnviada = mensagemEnviada.size();
+		DWORD tamanhoMensagemRecebida;
+		string mensagemRecebida;
+
+		bool k = WriteFile(hPipe, mensagemEnviada.c_str(), tamanhoMensagemEnviada, &tamanhoMensagemEnviada, NULL);
+
+		//Efetua a limpeza do buffer
+		memset(buffer,0,sizeof(buffer));
+
+		//Recebe a resposta do servidor
+		ReadFile(hPipe, buffer, TamanhoBuffer * sizeof(char), &tamanhoMensagemRecebida, NULL);
+
+		//Transforma a informação recebida do servidor em string
+		mensagemRecebida = string(buffer);
+
+		cout << "O servidor recebeu a mensagem em " << mensagemRecebida << endl;
+
+		cout << "Digite o texto a ser enviado: ";
+		getline(cin, mensagemEnviada);
+	}
+
+	return 0;
+}
---
+++ IPC/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Text;
+
+namespace Server
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Nome do Named Pipe
+			const string nomeDoPipe = "MeuPipe";
+
+			//Cria o Named Pipe
+			using (NamedPipeServerStream server = new NamedPipeServerStream(nomeDoPipe))
+			{
+				Console.WriteLine("Esperando conexão com o cliente...");
+
+				//Espera por uma conexão
+				server.WaitForConnection();
+
+				Console.WriteLine("Cliente conectado");
+
+				//Loop para que sempre que ele receber um pacote, ele entre em "modo de escuta" novamente
+				while (true)
+				{
+					byte[] buffer = new byte[512];
+
+					//Ao chamar o Read, o servidor fica na espera até ter uma informação disponível e seguidamente faz a escrita da mesma no buffer
+					int quantidadeDeBytes = server.Read(buffer, 0, buffer.Length);
+
+					//Se a quantidade recebida for 0, o cliente se desconectou e o servidor pode ser encerrado.
+					if (quantidadeDeBytes == 0)
+					{
+						break;
+					}
+
+					//Faz a conversão dos bytes recebidos em string
+					string textoRecebido = Encoding.UTF8.GetString(buffer, 0, quantidadeDeBytes);
+
+					//Transforma em maiúscula
+					string textoMaiusculo = textoRecebido.ToUpper();
+
+					//Mostra a informação recebida em maiúscula
+					Console.WriteLine($"Informação recebida: {textoMaiusculo}");
+
+					//Pega a data atual em string e armazena no array de bytes
+					byte[] dataAtual = Encoding.UTF8.GetBytes(DateTime.Now.ToString("dd/MM/yyyy HH:mm"));
+
+					//Envia os bytes para o cliente
+					server.Write(dataAtual, 0, dataAtual.Length);
+				}
+			}
+		}
+	}
+}
